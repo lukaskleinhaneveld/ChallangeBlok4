ba80874c9a256bfaafd1c9270870fe9dbe5025f5
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-06-28 14:44:40 +0200

    no message

--- view/templates/header.php
+++ view/templates/header.php
@@ -39,8 +39,6 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?= $_SESSION['Username'] ?> <span class="caret"></span></a>
 						<ul class="dropdown-menu">
-							<li><a href="<?= URL ?>Barber/account">Account</a></li>
-							<li><a href="<?= URL ?>Barber/afspraken">Mijn Afspraken</a></li>
 							<li><a href="<?= URL ?>Login/logout">Logout</a></li>
 						</ul>
 					</li>
