c5a6a0d0e04ee725202b145a15cd0c4947498c55
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-06-28 13:07:06 +0200

    Fixed Registration

--- model/RegisterModel.php
+++ model/RegisterModel.php
@@ -10,14 +10,14 @@ function registerDB(){
 	$Cell = isset($_POST['Cell']) ? $_POST['Cell'] : '';
     $Email = isset($_POST['Email']) ? $_POST['Email'] : '';
 	$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+	$isBarberer = isset($_POST['isBarberer']) ? $_POST['isBarberer'] : '0';
 
     // Hash the password
     $Password = hash('sha256', $Password);
-    if (!isset($Name) && !isset($Adress) && !isset($PostalCode) && !isset($City) && !isset($Phone) && !isset($Cell) && !isset($Email) && !isset($Password)) {
+    if (!isset($Name) && !isset($Adress) && !isset($PostalCode) && !isset($City) && !isset($Phone) && !isset($Cell) && !isset($Email) && !isset($Password) && !isset($isBarberer)) {
 		return "Not all fields have been filled in properly";
 	}
-
-	$sql = "INSERT INTO users (Name, Adress, PostalCode, City, Phone, Cell, Email, Password) VALUES (:Name, :Adress, :PostalCode, :City, :Phone, :Cell, :Email, :Password)";
+	$sql = "INSERT INTO users (Name, Adress, PostalCode, City, Phone, Cell, Email, Password, isBarberer) VALUES (:Name, :Adress, :PostalCode, :City, :Phone, :Cell, :Email, :Password, :isBarberer)";
 	$query = $db->prepare($sql);
 	$query->execute(array(
 		':Name' => $Name,
@@ -27,6 +27,7 @@ function registerDB(){
         ':Phone' => $Phone,
         ':Cell' => $Cell,
         ':Email' => $Email,
-        ':Password' => $Password
+        ':Password' => $Password,
+        'isBarberer' => $isBarberer
     ));
 }
--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,24 +1,24 @@
                         <hr>
-                        <div class="content-box">
+                        <footer>
                             <h2>Openingstijden</h2>
                             <p>
-                            <span class="day-name">Di</span>
-                            <span class="day-hours">08:30 - 20:00</span>
-                            <br>
-                            <span class="day-name">Wo</span>
-                            <span class="day-hours">08:30 - 12:30</span>/<span class="day-hours">13:30 - 17:30</span>
-                            <br>
-                            <span class="day-name">Do</span>
-                            <span class="day-hours">08:30 - 12:30</span>/<span class="day-hours">13:30 - 17:30</span>
-                            <br>
-                            <span class="day-name">Vr</span>
-                            <span class="day-hours">08:30 - 12:30</span>/<span class="day-hours">13:30 - 18:00</span>
-                            <br>
-                            <span class="day-name">Za</span>
-                            <span class="day-hours">08:30 - 15:10</span>
-                            <br>
-                        </p>
-                    </div>
+                                <span class="day-name">Di</span>
+                                <span class="day-hours">08:30 - 20:00</span>
+                                <br>
+                                <span class="day-name">Wo</span>
+                                <span class="day-hours">08:30 - 12:30</span>/<span class="day-hours">13:30 - 17:30</span>
+                                <br>
+                                <span class="day-name">Do</span>
+                                <span class="day-hours">08:30 - 12:30</span>/<span class="day-hours">13:30 - 17:30</span>
+                                <br>
+                                <span class="day-name">Vr</span>
+                                <span class="day-hours">08:30 - 12:30</span>/<span class="day-hours">13:30 - 18:00</span>
+                                <br>
+                                <span class="day-name">Za</span>
+                                <span class="day-hours">08:30 - 15:10</span>
+                                <br>
+                            </p>
+                        </footer>            
                 </div>
             </div>
         </div>
